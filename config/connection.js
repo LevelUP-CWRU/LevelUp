fb52834b46599ee53218347624431af706a0b394
Hayley Hartman <hayleyhartman@Hayleys-MacBook-Pro.local>
2018-11-20 18:05:27 -0500

    this sucks

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,22 @@
+// Set up MySQL connection.
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 8889,
+  port: 3306,
   user: "root",
-  password: "",
-  database: ""
+  password: "IJ-25-jo",
+  database: "levelup_db"
 });
 
-
-connection.connect(function(err) {
+// Make connection.
+connection.connect((err) => {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error(`error connecting: ${err.stack}`);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log(`connected as id ${connection.threadId}`);
 });
 
+// Export connection for our ORM to use.
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,20 +1,41 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const port = process.env.PORT || 8080;
+
+const passport = require('passport');
+const flash = require('connect-flash');
+
+require('./config/passport')(passport);
+
+app.use(express.static("public"))
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 const exphbs = require('express-handlebars')
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}))
+app.set("view engine", "handlebars");
 
-//if we choose to use handlebars
-// app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-// app.set('view engine', 'handlebars')
+app.use(session({
+    secret: 'levelupmakesecuritybetter',
+    resave: true,
+    saveUninitialized: true
+}));
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
-const PORT = process.env.PORT || 8000;
+require('./routes/index')(app, passport);
 
-const routes = require("./controllers/controller.js");
-
-app.use(routes);
-
-app.listen(PORT, function () {
-    console.log("Server listening on: http://localhost:" + PORT);
-});
+app.listen(port);
+console.log("Port: " + port);
